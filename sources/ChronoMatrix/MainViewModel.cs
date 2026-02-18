a1d93f4567bb616e151463aedfa8d7bf921c2ea0
Alexandru Iuga <alex.iuga@gmail.com>
2026-02-18 22:19:15 +0200

    Added version in the left bottom corner

--- sources/ChronoMatrix/MainViewModel.cs
+++ sources/ChronoMatrix/MainViewModel.cs
@@ -1,5 +1,13 @@
-﻿namespace DustInTheWind.ChronoMatrix;
+﻿using System.Reflection;
+
+namespace DustInTheWind.ChronoMatrix;
 
 public class MainViewModel : ViewModelBase
 {
+    public string Version { get; private set; }
+
+    public MainViewModel()
+    {
+        Version = Assembly.GetEntryAssembly()?.GetName().Version?.ToString(3);
+    }
 }
